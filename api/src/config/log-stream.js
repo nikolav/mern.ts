4a2bcd3ff8dae176ccca66572c246b89c179c4e4
nikolav <admin@nikolav.rs>
2022-10-31 18:50:43 +0100

    debug log-stream

--- api/src/config/log-stream.js
+++ api/src/config/log-stream.js
@@ -4,7 +4,11 @@ const { ACCESS_LOG, LOGS_PATH } = require('../config/vars');
 
 module.exports = (logPath = ACCESS_LOG) => {
   const stream = fs.createWriteStream(
-    path.join(__dirname, LOGS_PATH, logPath),
+    path.join(
+      // __dirname, 
+      LOGS_PATH,
+      logPath
+    ),
     { flags: 'a' }
   );
   return { stream };
